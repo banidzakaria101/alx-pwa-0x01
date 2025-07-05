405e92fe9ad4769e057dab873c55bdae71a12693
zakariabanid101 <banidzakaria2@gmail.com>
2025-07-06 00:47:53 +0100

    link the manifest in _document.js

---
+++ alx-movie-app/public/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
